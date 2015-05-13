448b1fbaab4a05abb1aa9ed32cbff14f69847504
HanhKieu <hckieu@ucdavis.edu>
2015-05-12 17:55:35 -0700

    Finished HSVFunction successfully

--- main.m
+++ main.m
@@ -1,55 +1,25 @@
 myImg = imread('myImg.jpg');
 
-k = 8;
-%UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here
-% rows = size(myImg,1);
-% columns = size(myImg,2);
-% 
-R = myImg(:,:,1);
-G = myImg(:,:,2);
-B = myImg(:,:,3);
-% 
-% % Rcluster = kmeans(R,);
-% % Gcluster = kmeans(G,k);
-% % Bcluster = kmeans(B,k);
-% 
-% [~, centerR] = kmeans(double(R(:)),k,'Distance','sqeuclidean','Replicates',3);
-% [~, centerG] = kmeans(double(G(:)),k,'Distance','sqeuclidean','Replicates',3);
-% [~, centerB] = kmeans(double(B(:)),k,'Distance','sqeuclidean','Replicates',3);
-% originImg = myImg
+outputImg = quantizeRGB(myImg,8);
+% imshow(outputImg);
 
-rows = size(myImg,1);
-columns = size(myImg,2);
 
-my = reshape(myImg,rows * columns,3);
+hsvImg = rgb2hsv(myImg)
+
+hue = hsvImg(:,:,1)
+rows = size(hue,1);
+columns = size(hue,2);
+my = reshape(hue,rows * columns,1);
 [idx,centerMatrix] = kmeans(double(my),k);
-
 idx = reshape(idx,rows,columns);
-
-centerMatrix
-
-k = 8;
-
-newMatrix = zeros(rows,columns,3);
-
 
 
 for i=1:1:k
     [x,y] = find(idx == i);
     for j=1:1:size(x,1)
-        myImg(x(j,1),y(j,1),1) = centerMatrix(i,1); %R
-        myImg(x(j,1),y(j,1),2) = centerMatrix(i,2); %G
-        myImg(x(j,1),y(j,1),3) = centerMatrix(i,3); %B
+        hue(x(j,1),y(j,1)) = centerMatrix(i,1); %R
     end
 end
 
-
-R2 = myImg(:,:,1);
-G2 = myImg(:,:,2);
-B2 = myImg(:,:,3);
-
-outputImg = uint8(myImg);
-
-imshow(outputImg)
-
+hsvImg(:,:,1) = hue
+imshow(hsv2rgb(hsvImg))
--- quantizeRGB.m
+++ quantizeRGB.m
@@ -1,4 +1,4 @@
-function [myImg] = quantizeRGB( myImg,k )
+function [outputImg] = quantizeRGB( myImg,k )
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
 % rows = size(myImg,1);
@@ -15,27 +15,29 @@ function [myImg] = quantizeRGB( myImg,k )
 % [~, centerR] = kmeans(double(R(:)),k,'Distance','sqeuclidean','Replicates',3);
 % [~, centerG] = kmeans(double(G(:)),k,'Distance','sqeuclidean','Replicates',3);
 % [~, centerB] = kmeans(double(B(:)),k,'Distance','sqeuclidean','Replicates',3);
+% originImg = myImg
 
 rows = size(myImg,1);
 columns = size(myImg,2);
 
 my = reshape(myImg,rows * columns,3);
-[idx,centerMatrix] = kmeans(double(my),k,'Distance','sqeuclidean','Replicates',3);
+[idx,centerMatrix] = kmeans(double(my),k);
 
-idx = reshape(idx,[rows,columns]);
+idx = reshape(idx,rows,columns);
 
-idx
+% newMatrix = zeros(rows,columns,3);
 
 
-% 
-% for i=1:1:k
-%     [x,y] = find(idx == i);
-%     for j=1:1:size(x,1)
-%         myImg(x(j,1),y(j,1),1) = centerMatrix(i,1); %R
-%         myImg(x(j,1),y(j,1),2) = centerMatrix(i,2); %G
-%         myImg(x(j,1),y(j,1),3) = centerMatrix(i,3); %B
-%     end
-% end
+
+for i=1:1:k
+    [x,y] = find(idx == i);
+    for j=1:1:size(x,1)
+        myImg(x(j,1),y(j,1),1) = centerMatrix(i,1); %R
+        myImg(x(j,1),y(j,1),2) = centerMatrix(i,2); %G
+        myImg(x(j,1),y(j,1),3) = centerMatrix(i,3); %B
+    end
+end
+
 
 outputImg = uint8(myImg);
 
