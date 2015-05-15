ef60f703d394a437f95311f990ce1ce5b9d5c164
HanhKieu <hckieu@ucdavis.edu>
2015-05-14 17:35:33 -0700

    did most of hough transform

--- computeQuantizationError.m
+++ computeQuantizationError.m
@@ -1,8 +1,9 @@
 function [ error ] = computeQuantizationError( myImg, quantizedImg )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-
- error = sum(sum(sum((quantizedImg - myImg).^2)));
+myImg = double(myImg);
+quantizedImg = double(quantizedImg);
+error = sum(sum(sum((quantizedImg - myImg).^2)));
 
 
 end
